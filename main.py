abf6ea68e1579b1fcec87e5feecd6c4fbe583298
MinJaeKim2796 <59433841+MinJaeKim2796@users.noreply.github.com>
2020-01-09 18:01:07 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import game
+
+#import time
+
+n = int( input("몇 개의 라인업을 실행하시겠습니까? ") )
+m = int( input("몇 번 실행하시겠습니까?(x1000000) ") )
+
+print("라인업 이름을 입력하세요.(엔터키로 구분)")
+
+E=list()
+
+for i in range(0,n):
+    E.append( input("Who\'s lineup? ") + 'lineup.txt' )
+
+for i in range(0,n):
+    for j in range(0,m):
+        game.simulation(E[i])
